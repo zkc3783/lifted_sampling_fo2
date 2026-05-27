b27693b6ef72177d1fa1761d5a68575365e2f427
MengQiaolan <528198651@qq.com>
2026-05-27 20:10:52 +0800

    add version 32

--- src/wfomc/algo/__init__.py
+++ src/wfomc/algo/__init__.py
@@ -3,17 +3,22 @@ from enum import Enum
 from .IncrementalWFOMC3 import incremental_wfomc3
 from .IncrementalWFOMC3 import incremental_wfoms3
 from .IncrementalWFOMC3 import analyze_all_sample
+from .IncrementalWFOMC32 import incremental_wfomc32
+from .IncrementalWFOMC32 import incremental_wfoms32
 __all__ = [
 
     "incremental_wfomc3",
     "incremental_wfoms3",
-    "analyze_all_sample"
+    "incremental_wfomc32",
+    "incremental_wfoms32",
+    "analyze_all_sample",
 ]
 
 
 class Algo(Enum):
   
     INCREMENTAL3 = 'incremental3'
+    INCREMENTAL32 = 'incremental32'
 
     def __str__(self):
         return self.value
